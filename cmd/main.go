48afacdd1cc6aa865012533d8fdcf7d38f7d5a48
labile <1a6i1e@gmail.com>
2022-01-08 17:24:13 +0300

    add parse items with cli arg -> generate_db

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"github.com/BurntSushi/toml"
+	"gorm.io/gorm"
+	"io/ioutil"
 	"log"
 	"meme-sorter/internal"
 	"meme-sorter/web"
+	"os"
 )
 
 var (
@@ -27,16 +30,43 @@ func main() {
 	}
 
 	db := internal.NewDB(Config)
+	Config.DB = db
 
 	if db.Migrate() != nil {
 		log.Fatal(err)
 	}
-	//todo: протестировать апи и залить сервис в проект
 
-	Config.DB = db
+	if flag.Arg(1) == "generate_db" {
+		parseImageAndAddDb(&Config)
+		os.Exit(0)
+	}
 
 	err = web.Start(&Config)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func parseImageAndAddDb(Config *internal.Config) {
+	dir, err := ioutil.ReadDir(Config.ItemsDir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range dir {
+		byteFile, err := ioutil.ReadFile(Config.ItemsDir + string(os.PathSeparator) + file.Name())
+		if err != nil {
+			panic(err)
+		}
+
+		err = Config.DB.Create(&internal.Meme{
+			Model: gorm.Model{},
+			Fun:   false,
+			Image: byteFile,
+		})
+
+		if err != nil {
+			panic(err)
+		}
 	}
 }
